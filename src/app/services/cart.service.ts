4a6fc2adb2824a91d2505af71465d3d2837d099a
ADM-CESUMAR\felipe.oliveira <felipegerolomo.unicesumae@gmail.com>
2018-02-15 16:30:41 -0200

    Corrigido o erro do count no cart service.

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -11,40 +11,28 @@ export class CartService {
   }
 
   getItem(item: Array<any>) {
-    // this.produto.push(item);
-    // console.log(item["id"]);
+    console.log(item);
     if (this.cart.length == 0) {
-      item.count = 1;
+      item["count"] = 1;
       this.cart.push(item);
-      // console.log(this.cart);
     } else {
       let repeat = false;
       for (let i = 0; i < this.cart.length; i++) {
         if (item["id"] == this.cart[i].id) {
           repeat = true;
           this.cart[i].count += 1;
-          // console.log("Já Existe");
-          // console.log(this.cart);
         }
       }
       if (!repeat) {
-        // console.log("Não Existe");
-        item.count = 1
+        item["count"] = 1;
         this.cart.push(item);
-        // console.log(this.cart);
       }
-
-      // console.log(this.produto[0].id);
-
-
     }
 
 
 
 
-    // console.log(tam);
-    // console.log(item);
-    // console.log(this.cart[0].id);
+
   }
 
 }
